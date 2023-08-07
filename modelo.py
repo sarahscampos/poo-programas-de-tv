8ee9ada0b188b32d96b159368842bd43c4706e6c
sarah-campos <sarahstephanycampos@gmail.com>
2023-08-07 10:25:57 -0300

    polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,26 +19,34 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Likes: {self._likes}"
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} min - Likes: {self._likes}"
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {self._likes}"
+
 
 
 vingadores = Filme("vingadores: guerra infinita", 2018, 160)
 vingadores.dar_like()
 vingadores.dar_like()
-print("Nome: {} - Ano: {} - Duração: {} - Likes: {}"
-      .format(vingadores.nome, vingadores.ano, vingadores.duracao, vingadores.likes))
 
 atlanta = Serie("atlanta", 2018, 2)
-print("Nome: {} - Ano: {} - Temporadas: {} - Likes: {}"
-      .format(atlanta.nome, atlanta.ano, atlanta.temporadas, atlanta.likes))
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
